b062bd67b250cf3a06f9df86f3d383cf60e2f984
Nikita <ifresh3d@gmail.com>
2025-05-14 20:23:24 +0500

    Fix: base route

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/TO-React-DO/',
+	base: '/To-React-Do/',
 	plugins: [react(), tailwindcss(), svgr()],
 })
