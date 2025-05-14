bc56810e93f098af81b4636d4977426f0e8c8ee1
Nikita <ifresh3d@gmail.com>
2025-05-14 20:05:57 +0500

    Style: fix bug with height

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoListHeader from './features/todos/TodoListHeader'
 
 function App() {
 	return (
-		<div className='px-3 py-5 mx-auto mb-10 rounded-md shadow-md h-svh mt max-w-1/2 bg-gray-50'>
+		<div className='px-3 py-5 mx-auto mb-10 rounded-md shadow-md max-h-svh mt min-w-1/2 bg-gray-50'>
 			<h1 className='text-4xl font-bold text-center'>
 				To
 				<span className='text-xs font-semibold uppercase text-cyan-400'>
